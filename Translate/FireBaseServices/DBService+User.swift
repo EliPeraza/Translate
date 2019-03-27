9144d044e0fe04b5a6730db8ee7e0f226dab7048
elizabethperaza@gmail.com <elizabethperaza@gmail.com>
2019-03-27 18:07:20 -0400

    commiting to merge

--- Translate/FireBaseServices/DBService+User.swift
+++ Translate/FireBaseServices/DBService+User.swift
@@ -7,3 +7,46 @@
 //
 
 import Foundation
+
+
+struct NDUsersCollectionKeys {
+  static let CollectionKey = "users"
+  static let UserIdKey = "userId"
+  static let DisplayNameKey = "displayName"
+  static let EmailKey = "email"
+  static let PhotoURLKey = "photoURL"
+  static let JoinedDateKey = "joinedDate"
+}
+
+extension DBService {
+  static public func createNDUser(user: NDUser, completion: @escaping (Error?) -> Void) {
+    firestoreDB.collection(NDUsersCollectionKeys.CollectionKey)
+      .document(user.userId)
+      .setData([ DishesCollectionKeys.UserIdKey : user.userId,
+                 NDUsersCollectionKeys.DisplayNameKey : user.displayName,
+                 NDUsersCollectionKeys.EmailKey       : user.email,
+                 NDUsersCollectionKeys.PhotoURLKey    : user.photoURL ?? "",
+                 NDUsersCollectionKeys.JoinedDateKey  : user.joinedDate
+      ]) { (error) in
+        if let error = error {
+          completion(error)
+        } else {
+          completion(nil)
+        }
+    }
+  }
+  
+  static public func fetchUser(userId: String, completion: @escaping (Error?, NDUser?) -> Void) {
+    DBService.firestoreDB
+      .collection(NDUsersCollectionKeys.CollectionKey)
+      .whereField(NDUsersCollectionKeys.UserIdKey, isEqualTo: userId)
+      .getDocuments { (snapshot, error) in
+        if let error = error {
+          completion(error, nil)
+        } else if let snapshot = snapshot?.documents.first {
+          let dishCreator = NDUser(dict: snapshot.data())
+          completion(nil, dishCreator)
+        }
+    }
+  }
+}
